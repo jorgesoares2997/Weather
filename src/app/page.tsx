e551b18074b3045e0470d9b8a4eb3801ad2d479e
jorge soares <jorgesoares@jorges-MacBook-Air.local>
2024-10-09 13:59:02 -0300

    all code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,156 @@
-import Image from "next/image";
+"use client";
+import { useEffect, useState } from "react";
+import axios from "axios"; // Importando a biblioteca Axios
+import {
+  FaDroplet,
+  FaLocationDot,
+  FaMagnifyingGlass,
+  FaWind,
+} from "react-icons/fa6";
+
+const apiKey = process.env.NEXT_PUBLIC_API_KEY;
+const accessKey = process.env.NEXT_PUBLIC_ACCESS_KEY;
+
+const UnsplashPhotos = ({ city, setBackground }: any) => {
+  useEffect(() => {
+    const fetchPhotos = async () => {
+      try {
+        const response = await axios.get(
+          `https://api.unsplash.com/search/photos?query=${city}&client_id=${accessKey}`
+        );
+        const data = response.data;
+
+        if (data.results.length > 0) {
+          setBackground(data.results[0].urls.regular); // Define a URL da imagem como o fundo
+        }
+      } catch (error) {
+        console.error("Error fetching photos:", error);
+      }
+    };
+
+    if (city) {
+      fetchPhotos();
+    }
+  }, [city, setBackground]);
+
+  return null; // Não precisamos retornar nada, apenas mudar o fundo.
+};
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [city, setCity] = useState("");
+  const [weatherData, setWeatherData] = useState<any>(null);
+  const [background, setBackground] = useState(""); // Estado para armazenar a URL do fundo
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+  const getWeatherData = async (city: string) => {
+    const apiWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}&lang=pt_br`;
+
+    try {
+      const res = await axios.get(apiWeatherURL);
+      setWeatherData(res.data);
+    } catch (error) {
+      console.error("Error fetching weather data:", error);
+      setWeatherData(null);
+    }
+  };
+
+  const handleSearch = (
+    e:
+      | React.MouseEvent<HTMLButtonElement>
+      | React.KeyboardEvent<HTMLInputElement>
+  ) => {
+    e.preventDefault();
+
+    if (!city) {
+      console.error("City input is empty");
+      return;
+    }
+
+    getWeatherData(city);
+  };
+
+  // Função que lida com a tecla Enter
+  const handleKeyPress = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      handleSearch(e); // Chama a função de pesquisa ao pressionar Enter
+    }
+  };
+
+  return (
+    <div
+      className="h-full w-full flex m-auto justify-center bg-cover bg-center"
+      style={{
+        backgroundImage: weatherData ? `url(${background})` : "none", // Exibe a imagem de fundo somente após a busca
+      }}
+    >
+      <div className="container m-auto h-auto w-1/3 bg-opacity-90 grid items-center justify-center p-4 rounded-lg">
+        {weatherData && (
+          <UnsplashPhotos city={city} setBackground={setBackground} /> // Somente busca a imagem após a pesquisa
+        )}
+        <div className="form p-4">
+          <h3 className="text-[#fff] text-xl mb-4">
+            Confira o clima de uma cidade:
+          </h3>
+          <div className="form-input-container flex items-center">
+            <input
+              type="text"
+              placeholder="Digite o nome da cidade"
+              id="city-input"
+              value={city}
+              onChange={(e) => setCity(e.target.value)}
+              onKeyDown={handleKeyPress} // Detecta a tecla Enter
+              className="p-2 border rounded text-[#000] flex-1"
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+            <button
+              id="search"
+              onClick={handleSearch}
+              className="ml-2 p-2 bg-blue-500 text-white rounded"
+            >
+              <FaMagnifyingGlass />
+            </button>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        {weatherData && (
+          <div id="weather-data" className="p-4 text-white">
+            <h2 className="text-2xl flex items-center">
+              <FaLocationDot className="mr-2" />
+              <div className="flex items-center justify-center m-auto">
+                <span id="city">{weatherData.name}</span>
+
+                <img
+                  src={`https://flagsapi.com/${weatherData.sys.country}/flat/64.png`}
+                  id="country"
+                  alt={`${weatherData.sys.country} flag`}
+                  className="ml-2"
+                />
+              </div>
+            </h2>
+            <p id="temperature" className="text-xl">
+              <span>{Math.round(weatherData.main.temp)}</span>&deg;C
+            </p>
+            <div id="description-container" className="flex items-center">
+              <p id="description" className="mr-2">
+                {weatherData.weather[0].description}
+              </p>
+              <img
+                src={`http://openweathermap.org/img/wn/${weatherData.weather[0].icon}.png`}
+                id="weather-icon"
+                alt={weatherData.weather[0].description}
+              />
+            </div>
+            <div id="details-container" className="mt-4">
+              <p id="humidity" className="flex items-center">
+                <FaDroplet className="mr-2" />
+                <span>{weatherData.main.humidity}%</span>
+              </p>
+              <span className="border-l h-12"></span>
+              <p id="wind" className="flex items-center mt-2">
+                <FaWind className="mr-2" />
+                <span>{weatherData.wind.speed} km/h</span>
+              </p>
+            </div>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,20 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx}',  // Páginas do Next.js
+    './src/components/**/*.{js,ts,jsx,tsx}',  // Componentes personalizados
+    './src/app/**/*.{js,ts,jsx,tsx}',  // Diretório app (se estiver usando a nova estrutura do Next.js)
   ],
   theme: {
     extend: {
+      // Você pode personalizar o tema aqui, por exemplo, adicionando novas cores, fontes etc.
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        nearxPurple: '#6B21A8',
       },
     },
   },
   plugins: [],
-};
-export default config;
+}
+
+export default config
